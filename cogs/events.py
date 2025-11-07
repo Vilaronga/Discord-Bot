d6a81492d52d4fd418cdd187f6103f4495ae4ce7
Lucas Vilaronga <lucasvilaronga@gmail.com>
2025-11-07 20:11:19 -0300

    feat(events): events update

--- cogs/events.py
+++ cogs/events.py
@@ -6,13 +6,23 @@ class Events(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener() #evento que é disparado quando um novo usuário se junta ao discord
-    async def on_member_join(member:discord.Member): #o parâmetro member armazena: member:discord.Member 
+    async def on_member_join(self, member:discord.Member): #o parâmetro member armazena: member:discord.Member 
         canal = commands.Bot.get_channel(1436358990069104821) #aqui é atribuido um canal específico para o bot enviar a mensagem de boas-vindas
         await canal.send(f'Bem vindo ao servidor {member.mention}!') #canal.send: envia a msg no canal específico mencionando o novo membro.
 
     @commands.Cog.listener()
-    async def on_reaction_add(reacao:discord.Reaction, membro:discord.Member):
-        await reacao.message.reply(f'O membro {membro.mention} reagiu a mensagem com {reacao.emoji}')
+    async def on_message(self, msg:discord.Message):
+        if msg.author.bot:
+            return
+        elif msg.content == 'Ok':
+            await msg.add_reaction('👌')
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reacao:discord.Reaction, membro:discord.Member):
+        if membro.bot:
+            return
+        else:
+            await reacao.message.reply(f'O membro {membro.mention} reagiu a mensagem com {reacao.emoji}')
 
 async def setup(bot):
     await bot.add_cog(Events(bot))

c7c92627b5dfbc7b0c59ffb81c6607427125d882
Lucas Vilaronga <lucasvilaronga@gmail.com>
2025-11-07 19:45:28 -0300

    feat(embeds): Criação do primeiro modelo embed

--- cogs/embeds.py
+++ cogs/embeds.py
@@ -2,9 +2,44 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+##########################################################################################################################################
+## Slash Commands - Mais utilizados
+
+#Invés de utilizar ctx - context, usaremos o interaction. Eles possuem os mesmos objetos, são parecidos no que fazem mas funcionam de forma diferente.
+
+#O interection não representa o contexto, mas sim uma interação.
+#Name define o nome do comando
+#Dscription define a descrição do comando que estará visível no discord para o usuário
+#Uma diferença que tem no Interaction e que não tem context, é o parametro ephemeral. 
+#Ephemeral determina que aquela resposta somente será visível para o usuário que realizou a interação. Ou seja, quem chamou o comando.
+
+#A limitação do interaction é que ele só pode mandar uma resposta, ou seja só pode haver um interact.response...
+#Caso a resposta demore mais de 3 segundo para sair, o bot dará um erro, então para contornar isso, utilizamos interact.response.defer()
+#para contornar isso, utilizamos o interact.followup.send(), onde ele entende que é uma continuação da mensagem.
+
 class Embeds(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @app_commands.command(name='github', description='Divulge o seu GitHub')
+    async def github(self, interact:discord.Interaction, seu_nome:str, url_github:str, descricao:str):
+        embed = discord.Embed()
+        embed.set_author(name=f'@{interact.user.name}')
+        embed.title = seu_nome.title()
+        embed.description = descricao.lower().capitalize()
+        imagem = discord.File('cogs/images/github_logo_black.png', 'github_logo_black.png')
+        embed.set_image(url='attachment://github_logo_black.png')
+        embed.set_thumbnail(url=f'{interact.user.avatar}')
+        embed.set_footer(text='Divulgação GitHub')
+        embed.color = 16777215
+
+        #botão
+        botao = discord.ui.Button(label='GitHub', style=discord.ButtonStyle.link, url=url_github)
+        view = discord.ui.View()
+        view.add_item(botao)
+    
+        await interact.response.defer(ephemeral=True)
+        await interact.followup.send(embed=embed, file=imagem, view=view)
 
 async def setup(bot):
     await bot.add_cog(Embeds(bot))
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import discord #importa toda a api do discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
-from datetime import time
 
 #Importação de token do arquivo .env
 load_dotenv()
@@ -12,7 +11,7 @@ TOKEN = os.getenv('BOT_TOKEN')
 intents = discord.Intents.all() #essa variável armazena TODAS as permissões do discord
 bot = commands.Bot(command_prefix='!', intents = intents) #variável objeto que armazena todo o bot
 
-#Ao atribuir commands.Bot para a variável bot, automaticamente o a classe bot cria um atributo bot.tree (onde se encontram todos os comandos)
+#Ao atribuir commands.Bot para a variável bot, automaticamente a classe bot cria um atributo bot.tree (onde se encontram todos os comandos)
 #em cada módulo do cogs, os comandos são criados à parte e somente são implementados bot.tree através do setup do módulo em bot.add_cogs
 
 #Importação de módulos pessoais
@@ -29,7 +28,7 @@ async def carregar_cogs():
 ##bot.load_extension('cogs.calculos')
 
 ##########################################################################################################################################
-## Events
+## Events 
 
 @bot.event  #criação de um evento que é disparado quando o bot estiver pronto
 async def on_ready():
@@ -38,34 +37,6 @@ async def on_ready():
     print(f'{len(synced_commands)} comandos sincronizados.')
     print('bot inicializado com sucesso')
 
-
-##########################################################################################################################################
-## Commands
-
-@bot.command()
-async def somar(ctx:commands.Context, num1:float, num2:float):
-    soma = num1 + num2
-    await ctx.reply(f'{ctx.author.mention}, a soma entre {num1} e {num2} é:  {soma}')
-
-@bot.command()
-async def ola(ctx:commands.Context):        #ctx variável que é do tipo Context - context - guarda todas as informações e processos relativos ao uso do comando, como por   exemplo canal em que foi chamado, quem chamou e etc.
-    nome = ctx.author.name
-    await ctx.reply(f'Olá, {nome}! Tudo bem?')
-
-##########################################################################################################################################
-## Slash Commands - Mais utilizados
-
-#Invés de utilizar ctx - context, usaremos o interaction. Eles possuem os mesmos objetos, são parecidos no que fazem mas funcionam de forma diferente.
-
-#O interection não representa o contexto, mas sim uma interação.
-#Name define o nome do comando
-#Dscription define a descrição do comando que estará visível no discord para o usuário
-#Uma diferença que tem no Interaction e que não tem context, é o parametro ephemeral. 
-#Ephemeral determina que aquela resposta somente será visível para o usuário que realizou a interação. Ou seja, quem chamou o comando.
-
-#A limitação do interaction é que ele só pode mandar uma resposta, ou seja só pode haver um interact.response...
-#Caso a resposta demore mais de 3 segundo para sair, o bot dará um erro, então para contornar isso, utilizamos interact.response.defer()
-#para contornar isso, utilizamos o interact.followup.send(), onde ele entende que é uma continuação da mensagem.
 '''
 @bot.tree.command(name='ola', description='Diz olá para o usuário.')
 async def ola(interact:discord.Interaction): 
@@ -80,79 +51,7 @@ async def par_ou_impar(interact:discord.Interaction, num1:int):
         res = 'impar'
     await interact.response.defer(ephemeral=True)
     await interact.followup.send(f'O número {num1} é {res}.', ephemeral=True)
-
-@bot.tree.command(name='github', description='Divulge o seu GitHub')
-async def github_slash(interact:discord.Interaction, url_github:str, seu_nome:str, descrição:str, url_linkedin:str):
-    embed = discord.Embed()
-    embed.set_author(name=seu_nome)
-    embed.title = 'Divulgação GitHub'
-    embed.description = descrição
-    imagem = discord.File('images/github_logo_black.png', 'github_logo_black.png')
-    embed.set_image(url='attachment://github_logo_black.png')
-    embed.set_thumbnail(url='attachment://github_logo_black.png')
-    embed.set_footer(text='Divulgação GitHub')
-    embed.color = 16777215
-
-    #botão
-    async def escolha(interaction:discord.Interaction):
-        escolha = interact.data['values']
-        await interaction.response.send_message('escolha')
-    botao = discord.ui.Button(label='GitHub', style=discord.ButtonStyle.link, url=url_github)
-    view = discord.ui.View()
-    view.add_item(botao)
-
-    botaoSelecao = discord.ui.Select(placeholder='Selecione uma rede social:')
-    opcoes = [
-        discord.SelectOption(label='GitHub', value='1', ),
-        discord.SelectOption(label='LinkedIn', value='2')
-    ]
-
-    botaoSelecao.options = opcoes
-
-    view.add_item(botaoSelecao)
-    
-    await interact.response.defer()
-    await interact.followup.send(embed=embed, file=imagem, view=view)'''
-
-##########################################################################################################################################
-## Embeds ##
-
-@bot.command()
-async def enviar_embed(ctx:commands.Context):
-    embed = discord.Embed() #criação do objeto embed
-    embed.title = 'Olá'
-    embed.description = 'Teste de criação de uma mensagem embed.'
-
-    imagem = discord.File('images/tropa.jpg', 'tropa.jpg') #criação de um objeto imagem, primeiro parametro é a localização e depois nomeando a imagem
-    embed.set_image(url='attachment://tropa.jpg')
-    embed.set_thumbnail(url='attachment://tropa.jpg')
-    embed.set_footer(text='Mensagem de teste')
-    embed.set_author(name='Lucas Vilaronga', icon_url=ctx.author.avatar)
-
-    #criação de botão
-    async def resposta_botao(interact:discord.Interaction):
-        await interact.response.send_message('Você clicou no botão')
-    view = discord.ui.View()
-    botao = discord.ui.Button(label='clique aqui', style=discord.ButtonStyle.grey)
-    botao.callback = resposta_botao
-    view.add_item(botao)
-
-
-    await ctx.reply(embed=embed, file=imagem, view=view)
-
-@bot.command()
-async def github(ctx:commands.Context):
-    embed = discord.Embed()
-    embed.title = 'Github - Lucas Vilaronga'
-    embed.description = 'Estudante do segundo período de Análise e Desenvolvimento de Sistemas pela Universidade Tirantes - UNIT. Confira meus projetos clicando na imagem abaixo.'
-    embed.url = 'https://github.com/Vilaronga?tab=repositories'
-    embed.set_author(name='Lucas Vilaronga', icon_url='https://avatars.githubusercontent.com/u/106574077?v=4&size=64')
-    imagem = discord.File('images/github_logo_black.png', 'github_logo_black.png')
-    embed.set_image(url='attachment://github_logo_black.png')
-    embed.set_thumbnail(url='attachment://github_logo_black.png')
-    embed.set_footer(text='Test footer')
-    embed.color = 16777215
-    await ctx.reply(embed=embed, file=imagem)
+'''
 
 ############################################################################################################################################
 bot.run(TOKEN)

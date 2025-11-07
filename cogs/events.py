2ca7c91031feede510a99a4f9d9a2fac6d66f716
Lucas Vilaronga <lucasvilaronga@gmail.com>
2025-11-07 19:05:48 -0300

    feat(events): fix timezone

--- cogs/events.py
+++ cogs/events.py
@@ -5,13 +5,6 @@ class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    '''@commands.Cog.listener()
-    async def on_message(msg:discord.Message):
-        if msg.author.bot:
-            return
-        await commands.Bot.process_commands(msg)
-        #await msg.reply(f'Olá {msg.author.mention}')'''
-
     @commands.Cog.listener() #evento que é disparado quando um novo usuário se junta ao discord
     async def on_member_join(member:discord.Member): #o parâmetro member armazena: member:discord.Member 
         canal = commands.Bot.get_channel(1436358990069104821) #aqui é atribuido um canal específico para o bot enviar a mensagem de boas-vindas
